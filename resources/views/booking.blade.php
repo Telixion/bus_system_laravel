0f7d07c75ca4df516becd91b70a85c4489792f5b
Telixion <hafeez5843@gmail.com>
2018-04-07 14:52:52 +0800

    initial commit

---
+++ resources/views/booking.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="booking">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				this is booking
+			</div>
+		</div>
+	</div>
+</section>
+@endsection
